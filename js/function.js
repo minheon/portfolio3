6f23ed2b011314560b17623c91f9ace9cdced80f
민헌 <hen1015@naver.com>
2022-07-16 18:15:57 +0900

    포트폴리오3

---
+++ js/function.js
@@ -0,0 +1,95 @@
+$(function(){
+  var $btnMnu = $('.btn_mnu');
+  var $gnb = $('.gnb')
+  var $indicator = $('.slide>.slide-pagination>li>a');
+  var $container = $('.slide>.slide-container');
+  var $link = $('.slide>.slide-container>li');
+  var $btnPrev = $('.slide>.prev');
+  var $btnNext = $('.slide>.next');
+  var intervalKey = null;
+  var nowIdx = 0;
+
+  var $btnLan = $('header>.lnb>p>a');
+  var $btnTop = $('footer>.top>a');
+    
+  function slideMove(){
+    $indicator.eq(nowIdx).parent().addClass('on').siblings().removeClass('on');
+    
+    $link.eq(nowIdx).find('a').addClass('active');
+    $link.parent().siblings().find('a').removeClass('active');
+
+    $container.stop().animate({left:-(nowIdx*100)+'%'},1000,'easeInOutCubic');
+  }
+
+  function nextIdx(){
+    if(nowIdx<3){
+      nowIdx++;
+    }else{
+      nowIdx = 0;
+    }
+  }
+
+  function autoPlay(){
+    intervalKey = setInterval(function(){
+      nextIdx();
+      slideMove();
+    },2300);
+  }
+
+  autoPlay();
+
+  function autoStop(){
+    clearInterval(intervalKey);
+  }
+
+  $indicator.on('click',function(event){
+    event.preventDefault();
+    autoStop();
+    nowIdx = $indicator.index(this)
+    
+    slideMove();
+  });
+    
+  $btnPrev.on('click',function(){
+    autoStop();
+
+    if(nowIdx>0){
+      nowIdx--;  
+    }else{
+      nowIdx = 3;
+    }
+
+    slideMove();
+  });
+
+  $btnNext.on('click',function(){
+    autoStop();
+    nextIdx();
+    slideMove();
+  });
+  //end of slide
+
+  //모바일 + button
+  $btnMnu.on('click',function(){
+    $(this).css({
+      transform : 'rotate(360deg)'
+    });
+
+    $gnb.slideToggle();
+  });
+
+  //language
+  $btnLan.on('click',function(event){
+    event.preventDefault();
+    $('header>.lnb>.language').stop().slideToggle();
+  });
+
+  //top button
+  $btnTop.on('click',function(event){
+    event.preventDefault();
+
+    $('html,body').stop().animate({
+      scrollTop : 0
+    },1000,'easeInOutCubic');
+  });
+});
